4ccf765575a3c2822e9b2def38ddd4c01cc76c81
JeremyDaug <jeremy.daug@gmail.com>
2020-01-12 01:29:19 -0600

    Cleaned Up usings

--- AstrologyCalculatorMain/AstrologyCalculator/TimespanUnits/TimespansTab/TimespansTabViewModel.cs
+++ AstrologyCalculatorMain/AstrologyCalculator/TimespanUnits/TimespansTab/TimespansTabViewModel.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
 
