1a9910a16e4099187fbfd95afa13b88687ea2222
JeremyDaug <jeremy.daug@gmail.com>
2020-01-25 00:09:53 -0600

    Sanity Update, Going to do a hard pass, and drop powered orbits entirely. It's not worth it.

--- AstrologyCalculatorMain/AstrologyCalculator/Body/NaturalOrbital.cs
+++ AstrologyCalculatorMain/AstrologyCalculator/Body/NaturalOrbital.cs
@@ -12,39 +12,6 @@ namespace AstrologyCalculator.Body
     /// </summary>
     public class NaturalOrbital : Orbital
     {
-        #region AdditionalProperties
-
-        /// <summary>
-        /// Mass of the parent body being orbited in kg.
-        /// </summary>
-        public double ParentMass { get; set; }
-
-        /// <summary>
-        /// The mass of the body that is orbiting in kg.
-        /// </summary>
-        public virtual double BodyMass { get; set; }
-
-        /// <summary>
-        /// The Gravity Constant in units of m^3 / (kg s^2)
-        /// </summary>
-        public const double G = 6.67408E-11;
-
-        /// <summary>
-        /// The Gravitational parameter of the body and it's parent.
-        /// </summary>
-        public double GParam => G * (ParentMass + BodyMass);
-
-        /// <summary>
-        /// The Velocity of the body at the current point.
-        /// </summary>
-        public double CurrentVelocity
-        {
-            get
-            {
-                return VelocityAtAnAngle(CurrentAngle);
-            }
-        }
-
         /// <summary>
         /// The orbital period of the body in seconds.
         /// Adusts the SemiMajorAxis of the orbit to remain consistent.
@@ -60,55 +27,5 @@ namespace AstrologyCalculator.Body
                 SemiMajorAxis = Math.Pow(Math.Pow(value / (2 * Math.PI), 2) * GParam, 1 / 3);
             }
         }
-
-        /// <summary>
-        /// Where the Barycenter (center of mass) of the system is.
-        /// Measured in distance from the center of the parent body in meters.
-        /// If the Barycenter is further from the center than the body's radius, 
-        /// then the orbit would be considered binary.
-        /// </summary>
-        public double Barycenter
-        {
-            get
-            {
-                return SemiMajorAxis / (1 + (ParentMass / BodyMass));
-            }
-        }
-
-        public Vector3D CurrentVelocityVector
-        {
-            get
-            {
-                var m = CurrentAngle;
-                var r = CurrentRadius;
-                var w = ArgumentOfPeriapsis;
-                var om = AscendingNode;
-                var i = Inclination;
-
-                var p = Math.Sqrt(GParam * SemiMajorAxis) / CurrentRadius *
-                    new Vector3D(-Math.Sin(EccentricAnomaly),
-                                 Math.Sqrt(1-Eccentricity*Eccentricity) * Math.Cos(EccentricAnomaly), 
-                                 0);
-
-                var result = Rotate_Z(-om) * Rotate_x(-Inclination) * Rotate_Z(-w);
-                return Vector3D.Multiply(p, result);
-            }
-        }
-
-        #endregion AdditionalProperties
-
-        #region HelperFunctions
-
-        /// <summary>
-        /// The Velocity of the Body at an angle in the orbit.
-        /// </summary>
-        /// <param name="radAngle">The position in orbit in Radians.</param>
-        /// <returns>The Velocity of the body along the orbital path in m/s^2</returns>
-        public double VelocityAtAnAngle(double radAngle)
-        {
-            return Math.Sqrt(GParam * (2 / RadiusAtAnAngle(radAngle) - 1 / SemiMajorAxis));
-        }
-
-        #endregion HelperFunctions
     }
 }
--- AstrologyCalculatorMain/AstrologyCalculator/Body/Orbital.cs
+++ AstrologyCalculatorMain/AstrologyCalculator/Body/Orbital.cs
@@ -9,13 +9,12 @@ using System.Windows.Media.Media3D;
 namespace AstrologyCalculator.Body
 {
     /// <summary>
-    /// Used for defining arbitrary orbits. It does not take Mass or gravity into acount.
+    /// Used for defining arbitrary orbits.
     /// Could be used to define any orbit (including powered orbit).
     /// </summary>
     public class Orbital
     {
         #region HelperFunctions
-
 
         /// <summary>
         /// Rotate about x axis
@@ -66,7 +65,6 @@ namespace AstrologyCalculator.Body
             return MeanMotion * time;
         }
 
-
         /// <summary>
         /// The Mean Anomaly (the fraction of an orbit's period that has elapsed since Periapsis), in Radians.
         /// </summary>
@@ -88,9 +86,53 @@ namespace AstrologyCalculator.Body
                 / (1 + Eccentricity * Math.Cos(radAngle));
         }
 
+        /// <summary>
+        /// The Natural Orbital Period, Assuming an Unpowered orbit.
+        /// </summary>
+        /// <returns>The Natural Orbital Period</returns>
+        public double NaturalOrbitalPeriod()
+        {
+            return 2 * Math.PI * Math.Sqrt(Math.Pow(SemiMajorAxis, 3) / GParam);
+        }
+
+        /// <summary>
+        /// The Velocity of the Body at an angle in the orbit.
+        /// </summary>
+        /// <param name="radAngle">The position in orbit in Radians.</param>
+        /// <returns>The Velocity of the body along the orbital path in m/s^2</returns>
+        public double VelocityAtAnAngle(double radAngle)
+        {
+            return Math.Sqrt(GParam * (2 / RadiusAtAnAngle(radAngle) - 1 / SemiMajorAxis)) * OrbitalPeriod / NaturalOrbitalPeriod();
+        }
+
+        /// <summary>
+        /// Gives the inner product of two vectors
+        /// </summary>
+        /// <param name="a">Vector 1</param>
+        /// <param name="b">Vector 2</param>
+        /// <returns>The Inner Product</returns>
+        private double InnerProduct(Vector3D a, Vector3D b)
+        {
+            return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        }
+
         #endregion HelperFunctions
 
         #region DerivedProperties
+
+        /// <summary>
+        /// Where the Barycenter (center of mass) of the system is.
+        /// Measured in distance from the center of the parent body in meters.
+        /// If the Barycenter is further from the center than the body's radius, 
+        /// then the orbit would be considered binary.
+        /// </summary>
+        public double Barycenter
+        {
+            get
+            {
+                return SemiMajorAxis / (1 + (ParentMass / BodyMass));
+            }
+        }
 
         /// <summary>
         /// The Angle from the center of the Ellipse to the current point.
@@ -182,14 +224,33 @@ namespace AstrologyCalculator.Body
             }
         }
 
+        /// <summary>
+        /// Gets the Acceleration towards the parent body in 
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public double AccelerationAtRadius(double r)
+        {
+            return G * ParentMass / Math.Pow(r, 2);
+        }
+
+        public double ParentMassBasedOnForceAtRadius(double a, double r)
+        {
+            return a * r * r / G;
+        }
+
         #endregion DerivedProperties
 
         #region StateVectorElements
 
+        /// <summary>
+        /// The Current Position Vector
+        /// </summary>
         public Vector3D CurrentPoint
         {
             get
             {
+                //https://downloads.rene-schwarz.com/download/M001-Keplerian_Orbit_Elements_to_Cartesian_State_Vectors.pdf
                 var m = CurrentAngle;
                 var r = CurrentRadius;
                 var w = ArgumentOfPeriapsis;
@@ -201,7 +262,129 @@ namespace AstrologyCalculator.Body
                 var result = Rotate_Z(-om) * Rotate_x(-Inclination) * Rotate_Z(-w);
                 return Vector3D.Multiply(p, result);
             }
+            set
+            {
+                // https://downloads.rene-schwarz.com/download/M002-Cartesian_State_Vectors_to_Keplerian_Orbit_Elements.pdf
+                // From State Vector to Keplerian Elements
+                var orbMomentumVec = Vector3D.CrossProduct(value, CurrentVelocityVector);
+                var eccVec = Vector3D.CrossProduct(CurrentVelocityVector, orbMomentumVec) / GParam
+                    - value / value.Length;
+
+                var transposition = new Vector3D(0, 0, 1);
+                var nToAscNode = Vector3D.CrossProduct(new Vector3D(0, 0, 1), orbMomentumVec);
+
+                if (InnerProduct(value, CurrentVelocityVector) >= 0)
+                    TrueAnomaly = Math.Acos(InnerProduct(eccVec, value) / (eccVec.Length * value.Length));
+                else
+                    TrueAnomaly = 2 * Math.PI - Math.Acos(InnerProduct(eccVec, value) / (eccVec.Length * value.Length));
+
+                Inclination = Math.Acos(orbMomentumVec.Z / orbMomentumVec.Length);
+
+                Eccentricity = eccVec.Length;
+
+                if (nToAscNode.Y >= 0) AscendingNode = Math.Acos(nToAscNode.Y / nToAscNode.Length);
+                else AscendingNode = 2 * Math.PI - Math.Acos(nToAscNode.Y / nToAscNode.Length);
+
+                if (eccVec.Z >= 0) ArgumentOfPeriapsis = Math.Acos(InnerProduct(nToAscNode, eccVec) / (nToAscNode.Length * eccVec.Length));
+                else ArgumentOfPeriapsis = 2 * Math.PI - Math.Acos(InnerProduct(nToAscNode, eccVec) / (nToAscNode.Length * eccVec.Length));
+
+                var EccAn = 2 * Math.Atan(Math.Tan(TrueAnomaly / 2) /
+                    Math.Sqrt((1 + Eccentricity) / (1 - Eccentricity)));
+
+                SemiMajorAxis = 1 / (2 / value.Length - CurrentVelocityVector.LengthSquared / GParam);
+            }
         }
+
+        /// <summary>
+        /// The Motion State Vector
+        /// </summary>
+        public Vector3D CurrentVelocityVector
+        {
+            get
+            {
+                // https://downloads.rene-schwarz.com/download/M001-Keplerian_Orbit_Elements_to_Cartesian_State_Vectors.pdf
+                var m = CurrentAngle;
+                var r = CurrentRadius;
+                var w = ArgumentOfPeriapsis;
+                var om = AscendingNode;
+                var i = Inclination;
+
+                var p = Math.Sqrt(GParam * SemiMajorAxis) / CurrentRadius *
+                    new Vector3D(-Math.Sin(EccentricAnomaly),
+                                 Math.Sqrt(1 - Eccentricity * Eccentricity) * Math.Cos(EccentricAnomaly),
+                                 0);
+
+                var result = Rotate_Z(-om) * Rotate_x(-Inclination) * Rotate_Z(-w);
+                return Vector3D.Multiply(p, result);
+            }
+            set
+            {
+                // https://downloads.rene-schwarz.com/download/M002-Cartesian_State_Vectors_to_Keplerian_Orbit_Elements.pdf
+                // From State Vector to Keplerian Elements
+                var orbMomentumVec = Vector3D.CrossProduct(CurrentPoint, value);
+                var eccVec = Vector3D.CrossProduct(value, orbMomentumVec) / GParam
+                    - CurrentPoint / CurrentPoint.Length;
+
+                var transposition = new Vector3D(0, 0, 1);
+                var nToAscNode = Vector3D.CrossProduct(new Vector3D(0, 0, 1), orbMomentumVec);
+
+                if (InnerProduct(CurrentPoint, value) >= 0)
+                    TrueAnomaly = Math.Acos(InnerProduct(eccVec, CurrentPoint) / (eccVec.Length * CurrentPoint.Length));
+                else
+                    TrueAnomaly = 2 * Math.PI - Math.Acos(InnerProduct(eccVec, CurrentPoint) / (eccVec.Length * CurrentPoint.Length));
+
+                Inclination = Math.Acos(orbMomentumVec.Z / orbMomentumVec.Length);
+
+                Eccentricity = eccVec.Length;
+
+                if (nToAscNode.Y >= 0) AscendingNode = Math.Acos(nToAscNode.Y / nToAscNode.Length);
+                else AscendingNode = 2 * Math.PI - Math.Acos(nToAscNode.Y / nToAscNode.Length);
+
+                if (eccVec.Z >= 0) ArgumentOfPeriapsis = Math.Acos(InnerProduct(nToAscNode, eccVec) / (nToAscNode.Length * eccVec.Length));
+                else ArgumentOfPeriapsis = 2 * Math.PI - Math.Acos(InnerProduct(nToAscNode, eccVec) / (nToAscNode.Length * eccVec.Length));
+
+                var EccAn = 2 * Math.Atan( Math.Tan(TrueAnomaly/2) /
+                    Math.Sqrt( (1 + Eccentricity) / (1 - Eccentricity) ));
+
+                SemiMajorAxis = 1 / (2/CurrentPoint.Length - value.LengthSquared/GParam);
+            }
+        }
+
+        /// <summary>
+        /// The Velocity of the body at the current point.
+        /// </summary>
+        public double CurrentVelocity
+        {
+            get
+            {
+                return CurrentVelocityVector.Length;
+            }
+            set
+            {
+                var mod = value / CurrentVelocityVector.Length;
+                CurrentVelocityVector = mod * CurrentVelocityVector;
+            }
+        }
+
+        /// <summary>
+        /// The Gravitational parameter of the body and it's parent.
+        /// </summary>
+        public double GParam => G * (ParentMass + BodyMass);
+
+        /// <summary>
+        /// Mass of the parent body being orbited in kg.
+        /// </summary>
+        public double ParentMass { get; set; }
+
+        /// <summary>
+        /// The mass of the body that is orbiting in kg.
+        /// </summary>
+        public double BodyMass { get; set; }
+
+        /// <summary>
+        /// The Gravity Constant in units of m^3 / (kg s^2)
+        /// </summary>
+        public const double G = 6.67408E-11;
 
         #endregion StateVectorElements
 
